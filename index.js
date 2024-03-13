31a17eb6b3dfad1d2258e191bb0cbeac7466a93e
Alesya Burnaeva <anburnaeva@gmail.com>
2024-03-13 13:25:17 +0300

    Добавлен экспорт и импорт

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import _ from "lodash";
 console.log("Hello, Hexlet!");
 console.log(_.last(["one", "two"]));
+
+import getBirthYear from "./src/index.js";
+
+console.log("Сколько вам лет?");
+const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from "lodash";
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
+
+export default (age) => {
+  const birthYear = 2005;
+
+  return birthYear;
+};
